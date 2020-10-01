1032f4bdfb7ffd307548b58a37a9b04ff5b2e344
cksooraj <soorajkumar04@gmail.com>
2020-10-01 17:33:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from 'react-router-dom';
+import Header from './Components/Header';
+import India from './Components/INDIA';
+import World from './Components/World';
+
+
+class App extends React.Component{
+
+  constructor(props){
+      super(props);
+      
+  }
+
+  render(){
+      return(
+          <div className="container-fluid">  
+            <Router>
+            <Header/>
+                <Switch>
+                
+                      <Route exact path="/">
+                             <India/>
+                       </Route>
+                      
+                       <Route path="/India">
+                              <India />
+                       </Route>
+
+                       <Route path="/World">
+                              <World />
+                       </Route>
+                </Switch>
+            </Router>
+
+          </div>
+      )
+  }
 }
-
-export default App;
+export default App
